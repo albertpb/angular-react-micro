fa1874567616995968d00146fb612493c428127d
Alberto Palumbo <palumbo.alb@gmail.com>
2022-10-10 15:55:39 -0300

    Add custom event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  public counter = 21;
+  public counter = 0;
 
-  public handleOnClick(stateCounter: number) {
+  public handleOnClick() {
     this.counter++;
   }
 }
--- src/components/my-react-component/MyReactComponentWrapper.tsx
+++ src/components/my-react-component/MyReactComponentWrapper.tsx
@@ -6,39 +6,46 @@ import {
   Input,
   OnChanges,
   OnDestroy,
+  OnInit,
   Output,
   SimpleChanges,
   ViewChild,
-  ViewEncapsulation
-} from '@angular/core';
-import { MyReactComponent } from 'src/components/my-react-component/MyReactComponent';
-import * as React from 'react';
+  ViewEncapsulation,
+} from "@angular/core";
+import { MyReactComponent } from "src/components/my-react-component/MyReactComponent";
+import * as React from "react";
 
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from "react-dom";
 
-const containerElementName = 'myReactComponentContainer';
+const containerElementName = "myReactComponentContainer";
 
 @Component({
-  selector: 'app-my-component',
+  selector: "app-my-component",
   template: `<span #${containerElementName}></span>`,
-  styleUrls: ['./MyReactComponent.scss'],
+  styleUrls: ["./MyReactComponent.scss"],
   encapsulation: ViewEncapsulation.None,
 })
-export class MyComponentWrapperComponent implements OnChanges, OnDestroy, AfterViewInit {
-  @ViewChild(containerElementName, {static: false}) containerRef: ElementRef;
+export class MyComponentWrapperComponent
+  implements OnChanges, OnDestroy, AfterViewInit, OnInit
+{
+  @ViewChild(containerElementName, { static: false }) containerRef: ElementRef;
 
   @Input() public counter = 10;
   @Output() public componentClick = new EventEmitter<void>();
+
+  public customEventCounter = new CustomEvent("counter");
 
   constructor() {
     this.handleDivClicked = this.handleDivClicked.bind(this);
   }
 
   public handleDivClicked() {
-    if (this.componentClick) {
-      this.componentClick.emit();
-      this.render();
-    }
+    console.log("handleDivClicked");
+    this.componentClick.emit();
+  }
+
+  public ngOnInit() {
+    window.addEventListener("counter", this.handleDivClicked);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -50,14 +57,27 @@ export class MyComponentWrapperComponent implements OnChanges, OnDestroy, AfterV
   }
 
   ngOnDestroy() {
-    ReactDOM.unmountComponentAtNode(this.containerRef.nativeElement);
+    if (this.containerRef) {
+      ReactDOM.unmountComponentAtNode(this.containerRef.nativeElement);
+    }
+
+    window.removeEventListener("counter", this.handleDivClicked);
   }
 
   private render() {
-    const {counter} = this;
+    const { counter, customEventCounter } = this;
 
-    ReactDOM.render(<div className={'i-am-classy'}>
-      <MyReactComponent counter={counter} onClick={this.handleDivClicked}/>
-    </div>, this.containerRef.nativeElement);
+    if (this.containerRef) {
+      ReactDOM.render(
+        <div className={"i-am-classy"}>
+          <MyReactComponent
+            onClick={this.handleDivClicked}
+            counter={counter}
+            customEventCounter={customEventCounter}
+          />
+        </div>,
+        this.containerRef.nativeElement
+      );
+    }
   }
 }
